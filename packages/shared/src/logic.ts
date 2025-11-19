ad9961214948d3adbf5275cd098fdf3a1daf31d9
Angamar <aleksa_95@msn.com>
2025-11-19 01:15:51 +0100

    chore: create several game logic functions

--- packages/shared/src/logic.ts
+++ packages/shared/src/logic.ts
@@ -1,4 +1,7 @@
-import { GameState } from "./types";
+import { GameState, Player } from "./types";
+import cards from "./cards";
+import { shuffle } from "./utils";
+import { evaluate } from "mathjs";
 
 export function createInitialGameState(): GameState {
   return {
@@ -18,5 +21,41 @@ export function nextRound(state: GameState): GameState {
   return {
     ...state,
     round: state.round + 1,
+  };
+}
+
+export function createInitialDeck() {
+  return shuffle(cards);
+}
+
+export function createPlayer(name: string) {
+  return {
+    name: name,
+    deck: createInitialDeck(),
+    hand: [],
+    discardPile: [],
+    helps: {
+      discards: 1,
+      draws: 2,
+    },
+    score: 0,
+  };
+}
+
+export function drawCard(player: Player) {
+  const card = player.deck[0];
+
+  return {
+    ...player,
+    deck: player.deck.slice(1),
+    hand: [card, ...player.hand],
+  };
+}
+
+export function evaluateMove(expression: string, target: number) {
+  const result = evaluate(expression);
+  return {
+    result,
+    isCorrect: result === target,
   };
 }
--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -2,3 +2,22 @@ export interface GameState {
   score: number;
   round: number;
 }
+
+export interface Card {
+  suit: string;
+  value: number | number[];
+  label: string;
+}
+
+export interface Helps {
+  discards: number;
+  draws: number;
+}
+export interface Player {
+  name: string;
+  deck: Card[];
+  hand: Card[];
+  discard: Card[];
+  score: number;
+  helps: Helps;
+}
